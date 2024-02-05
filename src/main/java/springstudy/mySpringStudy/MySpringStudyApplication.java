5b5d240cb60811d12b00a3c0456bdbed72184621
lys0611 <dldptmd611@gmail.com>
2024-02-05 22:56:37 +0900

    Initial commit

---
+++ src/main/java/springstudy/mySpringStudy/MySpringStudyApplication.java
@@ -0,0 +1,13 @@
+package springstudy.mySpringStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springstudy/mySpringStudy/MySpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudy.mySpringStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
