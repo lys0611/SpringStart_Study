cbb373509dd759f93ef8276ba150e6e1efe8a852
lys0611 <dldptmd611@gmail.com>
2024-02-06 02:07:13 +0900

    테스트 케이스 3 commit

--- src/test/java/springstudy/mySpringStudy/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/springstudy/mySpringStudy/repository/MemoryMemberRepositoryTest.java
@@ -19,4 +19,19 @@ class MemoryMemberRepositoryTest {
         Member result = repository.findById(member.getId()).get();
         assertThat(member).isEqualTo(result);
     }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
 }
