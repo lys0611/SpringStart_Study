939139a7af58d9c60c5c2900b09955e5fae3d268
lys0611 <dldptmd611@gmail.com>
2024-02-06 23:43:40 +0900

    회원 웹 기능 - 등록

--- src/main/java/springstudy/mySpringStudy/controller/MemberController.java
+++ src/main/java/springstudy/mySpringStudy/controller/MemberController.java
@@ -2,6 +2,9 @@ package springstudy.mySpringStudy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import springstudy.mySpringStudy.domain.Member;
 import springstudy.mySpringStudy.service.MemberService;
 
 @Controller
@@ -11,5 +14,24 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    //주로 조회 시 사용(데이터 조회 시)
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "/members/createMemberForm";
+    }
+
+    //주로 데이터를 폼 등에 넣어 전달할 경우 사용(데이터 등록 시)
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+//        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/springstudy/mySpringStudy/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package springstudy.mySpringStudy.controller;
+
+import java.util.SplittableRandom;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
