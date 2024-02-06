e6905df294960d6ff91f33941a051cc42c255db7
lys0611 <dldptmd611@gmail.com>
2024-02-06 20:40:31 +0900

    회원 서비스 테스트 3

--- src/test/java/springstudy/mySpringStudy/service/MemberServiceTest.java
+++ src/test/java/springstudy/mySpringStudy/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package springstudy.mySpringStudy.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import springstudy.mySpringStudy.domain.Member;
 import springstudy.mySpringStudy.repository.MemberRepository;
@@ -11,8 +12,14 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
-    MemberService memberService = new MemberService();
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
 
     @AfterEach
     public void afterEach() {
