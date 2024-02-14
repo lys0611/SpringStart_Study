56da6127172a4062cd41da488be6075108bed3ba
lys0611 <dldptmd611@gmail.com>
2024-02-14 21:23:33 +0900

    스프링 데이터 JPA commit

---
+++ src/main/java/springstudy/mySpringStudy/repository/SpringDataJpaRepository.java
@@ -0,0 +1,12 @@
+package springstudy.mySpringStudy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import springstudy.mySpringStudy.domain.Member;
+
+import java.util.Optional;
+
+public interface SpringDataJpaRepository extends JpaRepository<Member,Long>, MemberRepository {
+    //JPQL select m from Member m where m.name = ?
+    @Override
+    Optional<Member> findByName(String name);
+}
